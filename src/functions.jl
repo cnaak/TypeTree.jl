6618ab21942689d0757d7ced637195e6b111d5c2
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2021-07-07 13:44:12 -0300

    FIX formatting when excluding concrete types on src/functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -11,15 +11,12 @@ function tt(TY::Type, pref=("",); uni=true, concrete=false)
     LEN = length(pref) - 1
     PREF = LEN > 0 ? ( i == ELL ? SPC : i == FRK ? BAR : i for i in pref[1:LEN]) : ("",)
     append!(ret, ["$(join((PREF..., pref[end])))$(TY)\n"])
-    ST = subtypes(TY)
+    ST = [i for i in subtypes(TY) if isabstracttype(i) || concrete]
     LE = length(ST)
     for i in 1:LE
-        if isabstracttype(ST[i]) || concrete
-            append!(ret, tt(ST[i],
-                            (pref..., i < LE ? FRK : ELL),
-                            uni=uni,
-                            concrete=concrete))
-        end
+        append!(
+            ret, tt(ST[i], (pref..., i < LE ? FRK : ELL), uni=uni, concrete=concrete)
+        )
     end
     ret
 end
