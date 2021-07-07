eb898a55d6b98594aae5c63a63a8f76d69883126
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2021-07-07 16:34:08 -0300

    UPD tests on test/runtests.jl. All 12 tests passing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,19 +5,50 @@
 using typeTree
 using Test
 
+
+#==============================================================================================#
+#                                          Test Data                                           #
+#==============================================================================================#
+
+#----------------------------------------------------------------------------------------------#
+#                                        Abstract types                                        #
+#----------------------------------------------------------------------------------------------#
+
+abstract type tyTree <: Any end
+abstract type A <: tyTree end
+abstract type Aa <: A end
+abstract type Ab <: A end
+abstract type B <: tyTree end
+abstract type Ba <: B end
+
+
+#----------------------------------------------------------------------------------------------#
+#                                        Concrete types                                        #
+#----------------------------------------------------------------------------------------------#
+
+struct Aα <: A end
+struct Aaα <: Aa end
+struct Aaβ <: Aa end
+struct Abα <: Ab end
+
+
+#==============================================================================================#
+#                                            Tests                                             #
+#==============================================================================================#
+
 @testset "quiz.type.abstract:                                                     " begin
     # All types
-    @test length(tt(typeTree.A)) == 7
-    @test length(tt(typeTree.Aa)) == 3
-    @test length(tt(typeTree.Ab)) == 2
-    @test length(tt(typeTree.B)) == 2
-    @test length(tt(typeTree.Ba)) == 1
-    @test length(tt(typeTree.tyTree)) == 10
+    @test length(tt(A)) == 7
+    @test length(tt(Aa)) == 3
+    @test length(tt(Ab)) == 2
+    @test length(tt(B)) == 2
+    @test length(tt(Ba)) == 1
+    @test length(tt(tyTree)) == 10
     # Abstract types only
-    @test length(tt(typeTree.A, concrete=false)) == 3
-    @test length(tt(typeTree.Aa, concrete=false)) == 1
-    @test length(tt(typeTree.Ab, concrete=false)) == 1
-    @test length(tt(typeTree.B, concrete=false)) == 2
-    @test length(tt(typeTree.Ba, concrete=false)) == 1
-    @test length(tt(typeTree.tyTree, concrete=false)) == 6
+    @test length(tt(A, concrete=false)) == 3
+    @test length(tt(Aa, concrete=false)) == 1
+    @test length(tt(Ab, concrete=false)) == 1
+    @test length(tt(B, concrete=false)) == 2
+    @test length(tt(Ba, concrete=false)) == 1
+    @test length(tt(tyTree, concrete=false)) == 6
 end
